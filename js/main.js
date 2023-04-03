2cdb4a914f55c76a8edd2248c84567db14677722
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-03 17:38:24 -0300

    Exercise two

--- js/main.js
+++ js/main.js
@@ -1,39 +1,14 @@
 const $ = selector => document.querySelector(selector)
 const $$ = selector => document.querySelectorAll(selector)
 const create = element => document.createElement(element)
-//Exercise one PLUS: Traverse array of friends , add a sad face for each <li>
 
-//If I press the face, the friend is removed from the friend from the friends list
+const firstLetter = (values) =>{
+   let letters = ''
+   for(letter of values){
+      letters += letter.charAt(0)
+   }
+   return letters
 
-
-
-const showFriends = (values) =>{
-   const containerExercisePlus = $('.containerExerciseOnePlus')
-   console.log(containerExercisePlus)
-   const ul = create('ul')
-   containerExercisePlus.appendChild(ul)
-   values.forEach(value =>{
-      const li = create('li')
-      li.innerText += value
-      ul.appendChild(li)
-      const span = create('span')
-      li.appendChild(span)
-      span.classList.add('notFriend')
-      const img = create('img')
-      span.appendChild(img)
-      img.classList.add('imgSadFace')
-      img.setAttribute('src', './img/sadFace.png')
-      img.style.width = '17px'
-      const classImage = $$('.imgSadFace')
-      classImage.forEach(img =>{
-         img.addEventListener('click', function(){
-
-            //span is the parent element of img , li is the parent element of span.
-            console.log(img.parentElement.parentElement)
-            img.parentElement.parentElement.remove()
-         })
-      })
-   }) 
 }
 
-showFriends(['Mariana', 'Rosana', 'Federica'])
+console.log(firstLetter(['Mariana', 'Rosana', 'Federica']))
