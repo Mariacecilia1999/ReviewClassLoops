4b12ab1818aa324161ff235dae847799ba4883f9
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-03 08:09:11 -0300

    Capturing container and create ul

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 const $ = selector => document.querySelector(selector)
-
+const create = element => document.createElement(element)
 //Exercise one PLUS: Traverse array of friends , add a sad face for each <li>
 
 //If I press the face, the friend is removed from the friend from the friends list
 
+
+
 const showFriends = (values) =>{
-   
+   const containerExercisePlus = $('.containerExerciseOnePlus')
+   console.log(containerExercisePlus)
+   const ul = create('ul')
+   containerExercisePlus.appendChild(ul)
 }
 
 showFriends(['Mariana', 'Rosana', 'Federica'])
