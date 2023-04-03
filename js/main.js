7bc333ddcd3ec8db3060b7eabc915f0b55e8122f
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-03 08:43:45 -0300

    Addign delete friend

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const $ = selector => document.querySelector(selector)
+const $$ = selector => document.querySelectorAll(selector)
 const create = element => document.createElement(element)
 //Exercise one PLUS: Traverse array of friends , add a sad face for each <li>
 
@@ -23,6 +24,15 @@ const showFriends = (values) =>{
       img.classList.add('imgSadFace')
       img.setAttribute('src', './img/sadFace.png')
       img.style.width = '17px'
+      const classImage = $$('.imgSadFace')
+      classImage.forEach(img =>{
+         img.addEventListener('click', function(){
+
+            //First parentElement span, second parentElement li
+            console.log(img.parentElement.parentElement)
+            img.parentElement.parentElement.remove()
+         })
+      })
    }) 
 }
 
