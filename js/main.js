7effe059599b7204a21e24ce14f9cedf472acde0
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-03 07:43:02 -0300

    Addign the name to each <li> using 'innerText'

--- js/main.js
+++ js/main.js
@@ -5,10 +5,16 @@ const $ = selector => document.querySelector(selector)
 const friends = ['Mariana', 'Rosana', 'Federica']
 
 const showFriends = (array) =>{
+   const createUl = document.createElement('ul')
    array.forEach(showFriend =>{
       const container = $('.containerExerciseOne')
-      const createUl = document.createElement('ul')
+      const li = document.createElement('li')
+      li.innerText += showFriend
+
+
+
       container.appendChild(createUl)
+      createUl.appendChild(li)
       console.log(container)
    })
 }
