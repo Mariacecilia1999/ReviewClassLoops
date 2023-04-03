3aeb402d20f7377cc094be618467684dc8837ae7
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-03 07:26:40 -0300

    Addign exercise one without using DOM

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 const $ = selector => document.querySelector(selector)
+
+//Exercise one: Traverse array of friends
+
+const friends = ['Mariana', 'Rosana', 'Federica']
+
+const showFriends = (array) =>{
+   array.forEach(showFriend =>{
+      console.log(showFriend)
+   })
+}
+showFriends(friends)
